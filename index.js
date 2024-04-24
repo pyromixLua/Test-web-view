c57aceb3342038833288a45008af974a844a5bde
pyromixLua <86781393+pyromixLua@users.noreply.github.com>
2024-04-24 15:30:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var http = require("http").createServer(app);
 var io = require("socket.io")(http);
 app.use(express.static(__dirname + '/public'));
 // increase payload limit
-app.use(express.json({ limit: '50mb' }));
+//app.use(express.json({ limit: '50mb' }));
 
 var viewing = "none";
 
